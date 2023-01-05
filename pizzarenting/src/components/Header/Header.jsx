a14117621ce4deaf1eacc31afcb390c29b6578e5
Jose Rodriguez <rdrgz.jose123@gmail.com>
2023-01-05 23:02:43 +0100

    create: Header creado a falta de aplicarle un mejor diseño y estados

--- pizzarenting/src/components/Header/Header.jsx
+++ pizzarenting/src/components/Header/Header.jsx
@@ -2,33 +2,62 @@ import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
+import './Header.scss';
+const logged = false;
+
 
 function Header() {
-  return (
-    <Navbar bg="light" expand="lg">
-      <Container>
-        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#link">Link</Nav.Link>
-            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">
-                Another action
-              </NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.4">
-                Separated link
-              </NavDropdown.Item>
-            </NavDropdown>
-          </Nav>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
-  );
+  if (logged) {
+    return (
+      <Navbar className='navbarDesign ' bg="light" expand="lg">
+        <Container>
+          <Navbar.Brand href="#home" className='text-light'>Pizzer.IO</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="me-auto">
+              <Nav.Link href="#home" className='text-light'>Home</Nav.Link>
+              <Nav.Link href="#link" className='text-light'>Link</Nav.Link>
+              <NavDropdown title="" className='text-light' id="basic-nav-dropdown">
+                <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.2">
+                  Another action
+                </NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+                <NavDropdown.Divider />
+                <NavDropdown.Item href="#action/3.4">
+                  Separated link
+                </NavDropdown.Item>
+              </NavDropdown>
+            </Nav>
+            <Form className="d-flex">
+              
+              <Button variant="outline-light">Empezar</Button>
+            </Form>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    );
+  }else{
+    return (
+      <Navbar className='navbarDesign ' bg="light" expand="lg">
+        <Container>
+          <Navbar.Brand href="#home" className='text-light'>Pizzer.IO</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="me-auto">
+              
+            </Nav>
+            <Form className="d-flex">
+              
+              <Button variant="outline-light">Empezar</Button>
+            </Form>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    );
+  }
 }
 
 export default Header;
