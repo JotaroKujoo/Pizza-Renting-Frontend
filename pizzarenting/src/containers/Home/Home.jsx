fa2ace3a3ebbc99d4603594eed24dbdf169c4155
Jose Rodriguez <rdrgz.jose123@gmail.com>
2023-01-05 21:44:35 +0100

    start: Creating folder structure from react and basic app.js with basic home and header

---
+++ pizzarenting/src/containers/Home/Home.jsx
@@ -0,0 +1,43 @@
+import React from "react";
+import Card from 'react-bootstrap/Card';
+
+import { Container, Image, Row, Col } from "react-bootstrap";
+import {useNavigate} from "react-router-dom";
+import Button from 'react-bootstrap/Button';
+const pizzerias = ["Cheese Lovers","Meat Lovers","Veggie Lovers", "Giorno Giovana Pizzeria"];
+
+const Home = () => {
+
+
+    return (
+        <Container fluid className="">
+            <Row className="">
+                <Col>
+                    <div className="cardcontainer mt-3 d-flex justify-content-center align-items-center flex-wrap">
+                        
+                                {
+                                    pizzerias.map(pizzeria => (
+                                        <Card className="m-3" style={{ width: '18rem' }}>
+                                            <Card.Img variant="top" src="holder.js/100px180" />
+                                            <Card.Body>
+                                                <Card.Title>{pizzeria}</Card.Title>
+                                                <Card.Text>
+                                                Some quick example text to build on the card title and make up the
+                                                bulk of the card's content.
+                                                </Card.Text>
+                                                <Button variant="primary">Go somewhere</Button>
+                                            </Card.Body>
+                                        </Card>
+                                    ))
+                                }
+                                
+                            
+                    </div>
+                </Col>
+            </Row>
+        </Container>
+    )
+}
+
+export default Home;
+
