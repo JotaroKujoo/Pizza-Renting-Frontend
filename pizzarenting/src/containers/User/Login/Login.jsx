96e1f2830d89e6196e1d527e2a66791eceb4c44a
Jose Rodriguez <rdrgz.jose123@gmail.com>
2023-01-08 08:26:07 +0100

    add: Login y register funcionales con el backend deployado

--- pizzarenting/src/containers/User/Login/Login.jsx
+++ pizzarenting/src/containers/User/Login/Login.jsx
@@ -1,6 +1,11 @@
 import React, {useState, useEffect} from "react";
+
 import { useNavigate } from "react-router-dom";
 import { Col, Button, Row, Container, Card, Form } from 'react-bootstrap';
+import { errorCheck } from "../../../services/usefull";
+import { loginUser } from "../../../services/apicalls";
+import jwt_decode from "jwt-decode";
+
 
 const Login = () => {
     let navigate = useNavigate()
@@ -25,17 +30,41 @@ const Login = () => {
     }
     const errorHandler = (field, value, type) => {
         let error = ""
-        // error = errorCheck(field, value, type)
+        error = errorCheck(field, value, type)
         setUserError(((prevState) => ({
             ...prevState,
             [field + "Error"]: error
         })))
+        console.log(error)
     };
     const [user, setUser] = useState({
         mail: "",
         password: ""
     });
     const logMe = () =>{
+      try {
+        loginUser(user)
+        .then(res => {
+          if (res.data.message !== "Login successful"){
+            console.log("Algo va mal", res.data.message)
+            
+          }else{
+            sessionStorage.setItem("SAVEJWT", JSON.stringify(res.data.yourToken))
+            sessionStorage.setItem("SAVEUSERMAIL",JSON.stringify(user.mail))
+            if (res.data.role === "Admin"){
+              sessionStorage.setItem("SAVEUSERROL","Admin")
+            }else{
+              sessionStorage.setItem("SAVEUSERROL","userRole")
+            }
+          }
+          let decoded = jwt_decode(res.data.yourToken)
+          console.log(res,decoded)
+          navigate("/")
+          return res
+        })
+      } catch (error) {
+        console.log(error)
+      }
     }
     return (
         <Container>
@@ -53,18 +82,18 @@ const Login = () => {
                           <Form.Label className="text-center">
                             Email address
                           </Form.Label>
-                          <Form.Control type="email" name="mail" onChange={(e) => inputHandler(e)} placeholder="Enter email" />
+                          <Form.Control type="mail" name="mail" onChange={(e) => inputHandler(e)} onBlur={(e) => errorHandler(e.target.name, e.target.value, "mail")} placeholder="Enter email" />
                         </Form.Group>
   
                         <Form.Group className="mb-3" controlId="formBasicPassword">
                           <Form.Label>Password</Form.Label>
-                          <Form.Control type="password" name="password" onChange={(e) => inputHandler(e)} placeholder="Password" />
+                          <Form.Control type="password" name="password" onChange={(e) => inputHandler(e)} onBlur={(e) => errorHandler(e.target.name, e.target.value, "password")} placeholder="Password" />
                         </Form.Group>
                         <Form.Group className="mb-3" controlId="formBasicCheckbox"></Form.Group>
                         <div className="d-grid">
-                          <Button variant="primary" type="submit">
-                            Log in
-                          </Button>
+                          <div onClick={()=>logMe()} className="btn btn-primary">
+                          Log in
+                          </div>
                         </div>
                       </Form>
                       <div className="mt-3">
--- pizzarenting/src/containers/User/Register/Register.jsx
+++ pizzarenting/src/containers/User/Register/Register.jsx
@@ -1,6 +1,11 @@
 import React, {useState, useEffect} from "react";
 import { useNavigate } from "react-router-dom";
 import { Col, Button, Row, Container, Card, Form } from 'react-bootstrap';
+import { errorCheck } from "../../../services/usefull";
+import { registerUser } from "../../../services/apicalls";
+import { loginUser } from "../../../services/apicalls";
+import jwt_decode from "jwt-decode";
+
 
 const Register = () => {
     let navigate = useNavigate()
@@ -30,14 +35,30 @@ const Register = () => {
 
         let error = ""
 
-        // error = errorCheck(field, value, type)
+        error = errorCheck(field, value, type)
 
         setUserError(((prevState) => ({
             ...prevState,
             [field + "Error"]: error
 
         })))
+        console.log(error)
     };
+
+    const assertPasswordEqual = (pass) => {
+      let error = ""
+      if (user.password !== pass){
+        error = "Password dont match"
+      }
+
+
+        setUserError(((prevState) => ({
+            ...prevState,
+            ["Error"]: error
+
+        })))
+        console.log(error)
+  }
 
     const [user, setUser] = useState({
         name: "",
@@ -45,6 +66,44 @@ const Register = () => {
         password: "",
         passwordConfirm: "",
     });
+
+    const regMe = () => {
+      try {
+        registerUser(user)
+        .then(res=>{
+          console.log(res)
+          try {
+            loginUser(user)
+        .then(res => {
+          if (res.data.message !== "Login successful"){
+            console.log("Algo va mal", res.data.message)
+            
+          }else{
+            sessionStorage.setItem("SAVEJWT", JSON.stringify(res.data.yourToken))
+            sessionStorage.setItem("SAVEUSERMAIL",JSON.stringify(user.mail))
+            if (res.data.role === "Admin"){
+              sessionStorage.setItem("SAVEUSERROL","Admin")
+            }else{
+              sessionStorage.setItem("SAVEUSERROL","userRole")
+            }
+          }
+          let decoded = jwt_decode(res.data.yourToken)
+          console.log(res,decoded)
+          navigate("/")
+          return res
+        })
+            
+
+          } catch (error) {
+            console.log(error)
+          }
+        })
+      } catch (error) {
+        console.log(error)
+      }
+    }
+
+
     return (
             
         <Container>
@@ -60,14 +119,14 @@ const Register = () => {
                       <Form>
                         <Form.Group className="mb-3" controlId="Name">
                           <Form.Label className="text-center">Name</Form.Label>
-                          <Form.Control type="text" name="name" onChange={(e) => inputHandler(e)} placeholder="Enter Name" />
+                          <Form.Control type="text" name="name" onChange={(e) => inputHandler(e)} onBlur={(e) => errorHandler(e.target.name, e.target.value, "text")} placeholder="Enter Name" />
                         </Form.Group>
   
                         <Form.Group className="mb-3" controlId="formBasicEmail">
                           <Form.Label className="text-center">
                             Email address
                           </Form.Label>
-                          <Form.Control type="email" name="mail" onChange={(e) => inputHandler(e)} placeholder="Enter email" />
+                          <Form.Control type="mail" name="mail" onChange={(e) => inputHandler(e)} onBlur={(e) => errorHandler(e.target.name, e.target.value, "mail")} placeholder="Enter email" />
                         </Form.Group>
   
                         <Form.Group
@@ -75,23 +134,23 @@ const Register = () => {
                           controlId="formBasicPassword"
                         >
                           <Form.Label>Password</Form.Label>
-                          <Form.Control type="password" name="password" onChange={(e) => inputHandler(e)} placeholder="Password" />
+                          <Form.Control type="password" name="password" onChange={(e) => inputHandler(e)} onBlur={(e) => errorHandler(e.target.name, e.target.value, "password")} placeholder="Password" />
                         </Form.Group>
                         <Form.Group
                           className="mb-3"
                           controlId="formBasicPassword"
                         >
                           <Form.Label>Confirm Password</Form.Label>
-                          <Form.Control type="password" name="passwordConfirm" onChange={(e) => inputHandler(e)} placeholder="Password" />
+                          <Form.Control type="password" name="passwordConfirm" onChange={(e) => inputHandler(e)} onBlur={(e) => assertPasswordEqual(e.target.value)} placeholder="Password" />
                         </Form.Group>
                         <Form.Group
                           className="mb-3"
                           controlId="formBasicCheckbox"
                         ></Form.Group>
                         <div className="d-grid">
-                          <Button variant="primary" type="submit">
-                            Create Account
-                          </Button>
+                          <div onClick={()=>{regMe()}} className="btn btn-primary">
+                            Create account
+                          </div>
                         </div>
                       </Form>
                       <div className="mt-3">
