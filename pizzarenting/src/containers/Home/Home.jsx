65f0393db001d0ab675feb96ad6552f86cec8942
Jose Rodriguez <rdrgz.jose123@gmail.com>
2023-01-22 07:50:20 +0100

    add: Buscador funcional de pizzerias integrado

--- pizzarenting/src/containers/Home/Home.jsx
+++ pizzarenting/src/containers/Home/Home.jsx
@@ -5,7 +5,7 @@ import "./Home.scss"
 
 
 import { Container,Button, Row, Col,Form } from "react-bootstrap";
-import { bringAllPizzerias } from "../../services/apicalls";
+import { bringAllPizzerias, bringPizzeriasByName } from "../../services/apicalls";
 import PizzeriaCard from '../../components/Cards/PizzeriaCard';
 import BuscadorPizzeria from '../../components/Buscador/BuscadorPizzeria';
 
@@ -17,7 +17,7 @@ function Home(){
     })
 
     useEffect(()=>{
-        if (pizzerias.length===0){
+        if (pizzerias.length===0 || pizzerias === ""){
             setTimeout(()=>{
                 bringAllPizzerias()
                 .then((res)=>{
@@ -34,7 +34,17 @@ function Home(){
                 name: e.target.value
             }))
         },400)
+        bringPizzeriasByName(e.target.value).then((res)=>{setPizzerias(res.data.foundPizzeria)})
+        if (e.target.value === ""){
+            setTimeout(()=>{
+                bringAllPizzerias()
+                .then((res)=>{
+                    setPizzerias(res.data)
+                })
+            },300)
+        }
     }
+    console.log(pizzerias)
 
     return (
         <Container fluid className="homeContainer">
@@ -42,9 +52,9 @@ function Home(){
                 
                 <Col className='d-flex justify-content-center align-items-center flex-column' xs = {8}>
                      
-                <Form className="d-flex">
-                    <Form.Control onKeyUp={(e)=>{setTimeout(()=>{inputHandler(e)},400)}} placeholder="Busca una pizzeria" className="me-2"></Form.Control>
-                    <Button>Search</Button>
+                <Form className="d-flex w-75">
+                    <Form.Control onKeyUp={(e)=>{inputHandler(e)}} placeholder="Busca una pizzeria" className="me-2 p-2"></Form.Control>
+                   
                 </Form>
 
 
