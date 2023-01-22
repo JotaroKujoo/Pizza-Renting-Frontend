716d4d2625dca4938356dcff053c31b938dfb885
Jose Rodriguez <rdrgz.jose123@gmail.com>
2023-01-22 07:15:48 +0100

    Proyecto en MVP

--- pizzarenting/src/containers/User/Profile/UserOrders.jsx
+++ pizzarenting/src/containers/User/Profile/UserOrders.jsx
@@ -1,7 +1,7 @@
 import jwt_decode from "jwt-decode";
 import { useState } from "react";
 import { useEffect } from "react";
-import { Card } from "react-bootstrap";
+import { Card, Col, Container, Row } from "react-bootstrap";
 import { getMyOrders } from "../../../services/apicalls";
 import { bringPizzaById } from "../../../services/apicalls";
 function UserOrders(){
@@ -16,28 +16,35 @@ function UserOrders(){
             getMyOrders(parseInt(decoded.id)).then((res)=>setOrders(res.data.orders) )
         }
         orders.map((order)=>{
-            bringPizzaById(order.id).then((res)=>{console.log(res)})
+            bringPizzaById(order.pizzaId).then((res)=>{console.log(res)})
         })
         
     })
 
     if(orders.length !== 0){
         return(
-            <div>
-                {
-                    orders.map((order)=>{
-                        return(
-                            <Card>
-                                <Card.Header>{order.updatedAt.split("T")[0]} {order.updatedAt.split("T")[1].split(".")[0]}</Card.Header>
-                                <Card.Body>
-                                    {order.address}
-                                    {order.quantity}
-                                </Card.Body>
-                            </Card>
-                        )
-                    })
-                }
-            </div>
+            <Container fluid>
+                <Row>
+                    <h1 className="mb-5">Historial de pedidos</h1>
+                    <Col className="d-flex justify-content-center flex-wrap">
+                        
+                        {
+                            orders.map((order)=>{
+                                return(
+                                    <Card className="m-4" style={{ height: "8rem", width: '40rem' }}>
+                                        <Card.Header>{order.updatedAt.split("T")[0]} {order.updatedAt.split("T")[1].split(".")[0]}</Card.Header>
+                                        <Card.Body>
+                                            <div>{order.address}</div>
+                                            <div>{order.price}€</div>
+                                            
+                                        </Card.Body>
+                                    </Card>
+                                )
+                            })
+                        }
+                    </Col>
+                </Row>
+            </Container>
         )
     }
 
